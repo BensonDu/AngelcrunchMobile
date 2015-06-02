808f7abe801ff7ea178736c1b4e38976b1ec970e
BensonDu <dubaoxing@gmail.com>
2015-06-02 23:15:13 +0800

    com_list base.js

---
+++ static/js/wapsite/angelcrunch_base.js
@@ -0,0 +1,107 @@
+(function(){
+
+    this.base_url='http://angelcrunch.com/';
+
+    this.base_mobile='http://mobile.angelcrunch.com/';
+
+    this.api={
+        login:this.base_mobile+'v2/home/login',
+        comlist:this.base_mobile+'v2/m_startup'
+    };
+
+    //localStorage存储管理
+    this.base_local_data={
+
+        saveData:function(key,data){
+            var d=JSON.stringify(data);
+            localStorage.setItem(key,d);
+        },
+
+        getData:function(key){
+            var ret=null;
+            if(localStorage.hasOwnProperty(key)){
+                var data=localStorage[key];
+                try{
+                    ret=JSON.parse(data);
+                }
+                catch(e){
+                    ret=data;
+                }
+
+            }
+            return ret;
+        },
+
+        delData:function(key){
+            if(localStorage.hasOwnProperty(key)){
+                return localStorage.removeItem(key);
+            }
+        }
+    };
+    //跨域获取数据方法
+    this.base_remote_data={
+        ajaxjsonp:function(url,call,data){
+            $.ajax({
+                url:url,
+                type:'get',
+                cache:false,
+                data:data,
+                dataType:'jsonp',
+                jsonp:'callback',
+                success:function(ret) {
+                    var json={};
+                    if(typeof ret=='string'){
+                        try{
+                            json=JSON.parse(ret);
+                        }
+                        catch(e){
+
+                        }
+                    }
+                    if(typeof ret=='object'){
+                        json=ret;
+                    }
+                    call(json);
+                },
+                error:function(e){
+
+                }
+            });
+        }
+    };
+    //单页面列表，锚点数据
+    this.base_hash={
+
+        getdata:function(key){
+
+            var hash=location.hash,
+                array=hash.split('#'),
+                l=array.length,
+                main,
+                ml;
+
+            if(l>0 && l==2){
+                main=array[1].split('-');
+                ml=main.length;
+                for(var i=0;i<ml;i++){
+                    var d=main[i].split(':');
+                    if(d.length==2 && d[0]==key){
+                        return d[1];
+                    }
+                }
+            }
+            return false;
+        },
+        putdata:function(data){
+            var ret='#';
+            if(typeof data =='object'){
+                for(var i in data){
+                    if(typeof data[i] !== 'object' && typeof data[i] !== 'function'){
+                        ret+=i+':'+data[i]+'-';
+                    }
+                }
+            }
+            return ret.replace(/\-$/g,'');
+        }
+    };
+}).call(this);
