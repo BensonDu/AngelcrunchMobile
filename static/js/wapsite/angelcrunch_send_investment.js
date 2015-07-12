2c7ad001a30ad5bc5910902d052c715d634366b7
BensonDu <dubaoxing@gmail.com>
2015-07-12 11:06:46 +0800

    Make js scripts be clear

--- static/js/wapsite/angelcrunch_send_investment.js
+++ static/js/wapsite/angelcrunch_send_investment.js
@@ -218,3 +218,28 @@
         }
     });
 }).call(this);
+
+//过渡 协议同意
+(function () {
+    var className = {
+        container: ".mentos-container",
+        checkbox: ".mentos-container :checkbox",
+        // State
+        beChecked: "checked"
+    };
+    var _changeCheckboxModuleState = function () {
+        var $container = $($(this).closest(className.container));
+        if ($(this).is(":checked")) $container.addClass(className.beChecked);
+        else $container.removeClass(className.beChecked);
+    };
+
+    this.formModules = function () {
+        var $container, $checkbox;
+        $checkbox = $(className.checkbox);
+        $checkbox.change(function () {
+            _changeCheckboxModuleState.call(this);
+        });
+        _changeCheckboxModuleState.call($checkbox);
+    };
+    formModules();
+}).call(this);
